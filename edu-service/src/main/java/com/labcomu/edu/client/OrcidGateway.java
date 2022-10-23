4ca0e5c15c278270bfa7db312f4ba613b5110bd3
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2022-10-23 13:09:24 -0400

    Cleaned project

--- edu-service/src/main/java/com/labcomu/edu/client/OrcidGateway.java
+++ edu-service/src/main/java/com/labcomu/edu/client/OrcidGateway.java
@@ -2,8 +2,6 @@ package com.labcomu.edu.client;
 
 import com.labcomu.edu.configuration.EduProperties;
 import com.labcomu.edu.resource.Researcher;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -14,18 +12,15 @@ import javax.validation.constraints.NotNull;
 @Component
 @Validated
 public class OrcidGateway {
-    private static final String ID = "orcid";
     private final String fetchResearcherUrl;
-
-    private final ReactiveCircuitBreaker circuitBreaker;
 
     private final WebClient.Builder webClientBuilder;
 
     public OrcidGateway(final WebClient.Builder webClientBuilder,
-            final EduProperties properties, final ReactiveCircuitBreakerFactory<?, ?> factory) {
+            final EduProperties properties) {
         this.webClientBuilder = webClientBuilder;
         this.fetchResearcherUrl = properties.getUrl().getFetchResearcherDetails();
-        this.circuitBreaker = factory.create(ID);
+
     }
 
     public Researcher getResearcher(@NotNull final String orcid) {
@@ -35,7 +30,6 @@ public class OrcidGateway {
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Researcher.class)
-                .transform(circuitBreaker::run)
                 .block();
     }
 }
