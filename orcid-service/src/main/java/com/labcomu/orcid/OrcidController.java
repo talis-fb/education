955e85eca91ef8ebd8ca4b6fd74071b0744d4622
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2022-10-24 19:11:31 -0400

    Removed fault injection from clean branch

--- orcid-service/src/main/java/com/labcomu/orcid/OrcidController.java
+++ orcid-service/src/main/java/com/labcomu/orcid/OrcidController.java
@@ -1,12 +1,7 @@
 package com.labcomu.orcid;
 
-import com.labcomu.faultinjection.annotation.Delay;
-import com.labcomu.faultinjection.annotation.Handle;
-import com.labcomu.faultinjection.annotation.Mutate;
-import com.labcomu.faultinjection.api.Handler;
 import com.labcomu.orcid.resource.Researcher;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,24 +18,12 @@ public class OrcidController {
   private final OrcidService service;
 
   @GetMapping("active")
-  @Delay(seconds = 10)
   public boolean isActive() {
     return service.isActive();
   }
 
   @GetMapping("researcher/{orcid}")
-  @Handle(handler = ResearcherHandler.class)
-  @Mutate(field = "orcid", set="orcid + 'XXX'")
-  @Mutate(field = "educations[0].role", set="YYY")
   public Researcher getResearcher(@NotNull @PathVariable String orcid) {
     return service.getResearcher(orcid);
-  }
-
-  @Component
-  public static class ResearcherHandler implements Handler<Researcher> {
-    @Override
-    public void handle(Researcher researcher) {
-      researcher.setName(researcher.getName() + " [MUTATE]");
-    }
   }
 }
